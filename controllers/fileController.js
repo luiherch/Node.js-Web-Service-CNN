66fa67f0ef79368f5207a31c7d71dd9fd4a45851
luiherch <luiherch@teleco.upv.es>
2021-04-03 02:25:00 +0200

    archiver + fileController, ya comprime archivos

---
+++ controllers/fileController.js
@@ -0,0 +1,61 @@
+const fs = require ('fs');
+const path = require ('path');
+const archiver = require ('archiver');
+
+
+exports.createZip = () => {
+    //falta parametrizar el output
+    const output = fs.createWriteStream(process.cwd() + '/data/example.zip');
+    const archive = archiver('zip', {zlib: { level: 9 }});
+
+    // listen for all archive data to be written
+    // 'close' event is fired only when a file descriptor is involved
+    output.on('close', () => {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    output.on('end', () => {
+        console.log('Data has been drained');
+    });
+
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      // log warning
+    } else {
+      // throw error
+      throw err;
+    }
+    });
+
+    // good practice to catch this error explicitly
+    archive.on('error', (err) => {
+        throw err;
+    });
+
+    // pipe archive data to the file
+    archive.pipe(output);
+
+    // append files from a sub-directory and naming it `new-subdir` within the archive
+    //falta parametrizar el nombre de Audios 
+    archive.directory(process.cwd()+ '/audios', 'Audios');
+
+    // finalize the archive (ie we are done appending files but streams have to finish yet)
+    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+    archive.finalize();
+
+    //falta meter el nombre y path en bbdd
+}
+
+exports.sendZip = (req, res, next) => {
+    //streaming data
+    const file = fs.createReadStream(zipPath);
+    res.setHeader('Content-Type', 'application/zip');
+    res.setHeader('Content-Disposition', 'attachment; filename= "' + originalName +'.zip"');
+    archiver.
+    res.send(zipFile);
+    file.pipe(res);
+    //eliminar los archivos una vez enviados
+    fs.unlink(zipPath);
+}
--- routes/test.js
+++ routes/test.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 
 const controladorModelo = require('../controllers/modelController');
+const fileController = require('../controllers/fileController');
 
 // /test/model
 router.get('/model', controladorModelo.serveCNN);
+
+// /test/zip
+router.get('/zip', fileController.createZip);
 
 module.exports = router;
